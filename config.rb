f4c0678930b6cbba8dd1bbd4b802a95a0de6fc04
Simon VIELPEAU <simon@vielpeau.com>
2018-04-29 17:21:16 +0200

    correct changes

--- config.rb
+++ config.rb
@@ -17,8 +17,6 @@ configure :build do
   activate :relative_assets
   set :relative_links, true
   activate :sitemap, hostname: data.settings.site.url
-  activate :minify_css
-  activate :minify_javascript
   activate :minify_html, remove_input_attributes: false
 end
 
