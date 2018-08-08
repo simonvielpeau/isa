a9fc774b5b1a026562a9eaf23f788b2a716ea4c7
Simon Vielpeau <simon@vielpeau.com>
2018-08-08 08:14:04 +0200

    Add a working dropdown ;)

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,26 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown-isa").classList.toggle("show-isa");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  console.log("test");
+  if (!event.target.matches('.dropbtn-isa')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content-isa");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show-isa')) {
+        openDropdown.classList.remove('show-isa');
+      }
+    }
+  }
+}
